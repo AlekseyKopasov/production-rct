ce508e296a323134a732d9c9ea8f2f5a7ad07002
Алексей Копасов <alesey.k1@yandex.ru>
2025-03-18 17:03:07 +0300

    добавляет конфиг для роутов

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,10 +1,8 @@
 import './styles/index.scss'
-import { Suspense } from 'react';
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { AboutPage } from 'pages/AboutPage';
-import { MainPage } from 'pages/MainPage';
-import { Link, Route, Routes } from 'react-router-dom';
+import { Link } from 'react-router-dom';
+import { AppRouter } from './router';
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
@@ -16,12 +14,7 @@ const App = () => {
 
       <button onClick={toggleTheme}>TOGGLE</button>
 
-      <Suspense fallback={<div>Loading...</div>}>
-        <Routes>
-          <Route path='/' element={<MainPage />} />
-          <Route path='/about' element={<AboutPage />} />
-        </Routes>
-      </Suspense>
+      <AppRouter /> 
     </div>
   )
 }
