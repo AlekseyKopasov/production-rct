658372459b42bbc321186dbf85830e9cbd1a2ff2
Алексей Копасов <alesey.k1@yandex.ru>
2025-03-19 11:14:27 +0300

    добавляет Navbar

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,16 +1,15 @@
 import './styles/index.scss'
 import { classNames } from 'shared/lib/classNames/classNames';
 import { useTheme } from 'app/providers/ThemeProvider';
-import { Link } from 'react-router-dom';
 import { AppRouter } from './router';
+import { Navbar } from 'widgets/Navbar';
 
 const App = () => {
   const {theme, toggleTheme} = useTheme()
 
   return (
     <div className={classNames('app', {}, [theme])}>
-      <Link to={'/'}>Main</Link>
-      <Link to={'/about'}>About</Link>
+      <Navbar />
 
       <button onClick={toggleTheme}>TOGGLE</button>
 
